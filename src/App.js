9dd5e163a35d3f5ee2cb1ade08cbf5f401e6ef91
Alexandre Cuva <alexandre.cuva@finix.asia>
2019-10-20 11:11:44 +0200

    Burger layout done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+
 import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder/>
       </Layout>
     </div>
   );
