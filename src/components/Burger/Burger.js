533767cfa4782b171ee19e36df543f2406684d71
Alexandre Cuva <alexandre.cuva@finix.asia>
2019-10-20 16:24:03 +0200

    Build Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,16 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const Burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient type={igKey} key={igKey + 1}/>;
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={"bread-top"}/>
-            <BurgerIngredient type={"cheese"}/>
-            <BurgerIngredient type={"meat"}/>
+            {transformedIngredients}
             <BurgerIngredient type={"bread-bottom"}/>
         </div>
     );
